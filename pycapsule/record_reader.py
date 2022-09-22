3cf4998a685fc478eb813b10e11388161f5b93ce
Rafael Grigoryan <grraph.bio@gmail.com>
2022-09-22 15:55:21 +0300

    updated for interoperability with never datafile versions
    - added eegFileType class for filetype parameters
    - automatic detection of binary and JSON metadata files

--- pycapsule/record_reader.py
+++ pycapsule/record_reader.py
@@ -1,18 +1,36 @@
 import numpy as np
 import pathlib as pl
 import msgpack
+import json
 import struct
 import os.path
 
+from regex import F
+
 from .types import *
 
-def getEEGfromBinary(dataBytes, numChannels):
-    unpackFormat = "=d{0}".format("f" * numChannels)
+
+class eegFileType():
+    def __init__(self, version:int, numChannels:int):
+        if version == 0:
+            self.unpackFormat = "=d{0}".format("f" * numChannels)
+            self.timestamp_resolution = 1
+            self.RawEEG_dtype = "I"
+            self.RawEEG_dataBytes = 4
+            
+        elif version == 1:
+            self.unpackFormat = "=q{0}".format("f" * numChannels)
+            self.timestamp_resolution = 1e-6
+            self.RawEEG_dtype = "H"
+            self.RawEEG_dataBytes = 2
+
+
+def getEEGfromBinary(dataBytes, numChannels, ftype_version):
+    unpackFormat = ftype_version.unpackFormat
     eegReadData = list(struct.iter_unpack(unpackFormat, dataBytes)) 
     eegPreped = [i[1:(numChannels+1)] for i in eegReadData] # skip timestamp and pack
     eegNumpy = np.asarray(eegPreped).T
-    eegTimestamps = np.asarray([i[0] for i in eegReadData])
-
+    eegTimestamps = np.asarray([i[0] for i in eegReadData])*ftype_version.timestamp_resolution
     return eegNumpy, eegTimestamps
 
 def getResFromBinary(dataBytes, numChannels):
@@ -33,7 +51,7 @@ class RecordReaderVisitor:
         pass
     def OnEnd(self):
         pass
-    def OnRawEEG(self, eegData:np.ndarray, eegTimestamps:np.array):
+    def OnRawEEG(self, eegData:np.ndarray, eegTimestamps:np.ndarray):
         pass
     def OnRawResistance(self, resData:np.ndarray):
         pass
@@ -67,17 +85,17 @@ class BasicReaderVisitor(RecordReaderVisitor):
         self.stimuliLabels      = np.hstack(self.stimuliLabels)     if self.stimuliLabels       else None
         self.stimuliIds         = np.hstack(self.stimuliIds)        if self.stimuliIds          else None
 
-    def OnRawEEG(self, eegData:np.ndarray, eegTimestamps:np.array):
-        self.eegData.append(eegData)
-        self.eegTimestamps.append(eegTimestamps)
+    def OnRawEEG(self, eegData:np.ndarray, eegTimestamps:np.ndarray):
+        self.eegData.append(eegData)  # type: ignore
+        self.eegTimestamps.append(eegTimestamps)  # type: ignore
         
     def OnP300ProcessingUnit(self, p300unit:P300ProcessingUnit):
         targetStimulus = p300unit.targetStimulus
 
         for stimulusData in p300unit.stimuliData:
-            self.stimuliTimestamps.append(stimulusData.timestamp)
-            self.stimuliLabels.append(int(stimulusData.stimulusId == targetStimulus) if targetStimulus != UNDEFINED_STIMULUS else UNDEFINED_STIMULUS)
-            self.stimuliIds.append(stimulusData.stimulusId)
+            self.stimuliTimestamps.append(stimulusData.timestamp)  # type: ignore
+            self.stimuliLabels.append(int(stimulusData.stimulusId == targetStimulus) if targetStimulus != UNDEFINED_STIMULUS else UNDEFINED_STIMULUS)  # type: ignore
+            self.stimuliIds.append(stimulusData.stimulusId)  # type: ignore
 
 class RecordReader:
     def __init__(self, filepath):
@@ -98,12 +116,21 @@ class RecordReader:
 
         if not os.path.isfile(datFilepath):
             raise Exception("Session metadata .dat file was not found, unable to unpack metadata!") 
+        data = None
 
-        with open(datFilepath,'rb') as file:
-            data = msgpack.unpackb(file.read())
-            return data
+        try:
+            with open(datFilepath,'r') as file:
+                data = json.loads(file.read())
+                if 'eeg_file_version' not in list(data.keys()):
+                    data['eeg_file_version'] = 1
+        except UnicodeDecodeError:
+            with open(datFilepath,'rb') as file:
+                data = msgpack.unpackb(file.read())
+                if 'eeg_file_version' not in list(data.keys()):
+                    data['eeg_file_version'] = 0  
+        return data
 
-        return None
+        
 
 
     @staticmethod
@@ -145,6 +172,7 @@ class RecordReader:
 
         sessionMetadata = RecordReader.UnpackMetadata(datFilepath)
         numChannels = sessionMetadata["deviceInfo"]["numChannels"]
+        ftype_version = eegFileType(sessionMetadata['eeg_file_version'], numChannels)
 
         with open(recFilepath,'rb') as file:
             if not RecordReader.__ReadMagic(file):
@@ -177,15 +205,16 @@ class RecordReader:
                     eegBinaryData = punit[10]
                     stimuliBinaryData = punit[12]
 
-                    eegNumpy, eegTimestamps = getEEGfromBinary(eegBinaryData, numChannels)
+                    eegNumpy, eegTimestamps = getEEGfromBinary(eegBinaryData, numChannels, ftype_version)
                     stimuliData = getStimuliFromBinary(stimuliBinaryData)
 
                     visitor.OnP300ProcessingUnit(P300ProcessingUnit(unitId, actId, targetStimulus, stimuliCount, eegNumpy, eegTimestamps, stimuliData, shouldEndLearn))
                 elif packetType == CSRPacketType.RawEEG:
-                    numSamples = struct.unpack("I", file.read(4))[0]
-                    eegBinaryData = file.read(packetSize - 4)
+                    numSamples = struct.unpack(ftype_version.RawEEG_dtype,
+                        file.read(ftype_version.RawEEG_dataBytes))[0]
+                    eegBinaryData = file.read(packetSize - ftype_version.RawEEG_dataBytes)
                     
-                    eegNumpy, eegTimestamps = getEEGfromBinary(eegBinaryData, numChannels)
+                    eegNumpy, eegTimestamps = getEEGfromBinary(eegBinaryData, numChannels, ftype_version)
                     assert eegNumpy.shape[1] == numSamples
 
                     visitor.OnRawEEG(eegNumpy, eegTimestamps)
